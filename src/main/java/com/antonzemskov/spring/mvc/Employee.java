a6e15a6aab5de821a4190ddea3e63afb3acd8946
Anton <zantn22@gmail.com>
2021-01-17 21:22:40 +0300

    SpringMVC form:radiobutton AND form:checkbox

--- src/main/java/com/antonzemskov/spring/mvc/Employee.java
+++ src/main/java/com/antonzemskov/spring/mvc/Employee.java
@@ -9,12 +9,26 @@ public class Employee {
     private int salary;
     private String department;
     private Map<String, String> departments;
+    private String carBrand;
+    private Map<String, String> carBrands;
+    private String languages;
+    private Map<String, String> languageList;
 
     public Employee() {
         departments = new HashMap<>();
         departments.put("IT", "Information Technology");
         departments.put("HR", "Human Resources");
         departments.put("Sales", "Sales");
+
+        carBrands = new HashMap<>();
+        carBrands.put("BMW", "BMW");
+        carBrands.put("Audi", "Audi");
+        carBrands.put("MB", "MB");
+
+        languageList = new HashMap<>();
+        languageList.put("English", "EN");
+        languageList.put("Deutch", "DE");
+        languageList.put("French", "FR");
     }
 
     public String getName() {
@@ -55,6 +69,38 @@ public class Employee {
 
     public void setDepartments(Map<String, String> departments) {
         this.departments = departments;
+    }
+
+    public String getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(String carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public Map<String, String> getCarBrands() {
+        return carBrands;
+    }
+
+    public void setCarBrands(Map<String, String> carBrands) {
+        this.carBrands = carBrands;
+    }
+
+    public String getLanguages() {
+        return languages;
+    }
+
+    public Map<String, String> getLanguageList() {
+        return languageList;
+    }
+
+    public void setLanguageList(Map<String, String> languageList) {
+        this.languageList = languageList;
+    }
+
+    public void setLanguages(String languages) {
+        this.languages = languages;
     }
 
     @Override
--- src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
+++ src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 
 <html>
@@ -15,5 +16,16 @@ Your surname: ${employee.surname}
 Your salary: ${employee.salary}
 <br>
 Your department: ${employee.department}
+<br>
+Your department: ${employee.carBrand}
+<br>
+Language(s):
+<ul>
+    <c:forEach var="lang" items ="${employee.languages}">
+
+        <li> ${lang} </li>
+
+    </c:forEach>
+</ul>
 </body>
 </html>
