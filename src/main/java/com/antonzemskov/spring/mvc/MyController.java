6f4cf973c18213e11f9e106eaa9be70616c36857
Anton <zantn22@gmail.com>
2021-01-17 23:18:45 +0300

    SpringMVC Validation form

--- src/main/java/com/antonzemskov/spring/mvc/MyController.java
+++ src/main/java/com/antonzemskov/spring/mvc/MyController.java
@@ -2,11 +2,13 @@ package com.antonzemskov.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -40,8 +42,11 @@ public class MyController {
     }
 
     @RequestMapping("/showDetails")
-    public String showEmpDetails(@ModelAttribute("employee") Employee emp) {
+    public String showEmpDetails(@Valid @ModelAttribute("employee") Employee emp, BindingResult bindingResult) {
 
+        if(bindingResult.hasErrors()) {
+            return "ask-emp-details-view";
+        }
         String name = emp.getName();
         emp.setName("Mr" + name);
 
